9a562340efa88533b3e9e5f7157ae342d8a64f7b
Captain <kmcudia@student.hau.edu.ph>
2026-07-08 16:39:07 +0800

    Added list, detail, personal info templates and updated README.md

--- projects/urls.py
+++ projects/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Quiz 1 static page
     path('', views.projects_page, name='projects'),
+
+    # Quiz 2 dynamic views
+    path('list/', views.list_view, name='project_list'),
+    path('<int:project_id>/', views.detail_view, name='project_detail'),
+    path('personal-info/', views.personal_info_view, name='personal_info'),
 ]
